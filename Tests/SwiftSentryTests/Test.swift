ef90d15aa386d68a09ab352e0e775c9f81747cc6
Petr Pavlik <petrpavlik@me.com>
2025-02-21 15:29:59 +0100

    - Enable strict concurrency checking - Add support for error sampling - Add support to edit and filter out evetns before send - Code formating

---
+++ Tests/SwiftSentryTests/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  SwiftSentry
+//
+//  Created by Petr Pavlik on 13.11.2024.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
